3f11c49649d520776c982349cda22e0665d31cd3
ORTIZJIMENEZANTONIO <joseantonioescom@gmail.com>
2022-10-18 09:32:23 -0500

    tags deleted

--- dist/modules/sera/comer-agreement-events/dto/comer-agreement-events.dto.js
+++ dist/modules/sera/comer-agreement-events/dto/comer-agreement-events.dto.js
@@ -1,58 +1,7 @@
 "use strict";
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.ComerConvEventDto = void 0;
-const class_validator_1 = require("class-validator");
 class ComerConvEventDto {
 }
-__decorate([
-    (0, class_validator_1.IsNumber)(),
-    (0, class_validator_1.IsPositive)(),
-    (0, class_validator_1.IsNotEmpty)(),
-    __metadata("design:type", Number)
-], ComerConvEventDto.prototype, "eventId", void 0);
-__decorate([
-    (0, class_validator_1.IsNumber)(),
-    (0, class_validator_1.IsPositive)(),
-    (0, class_validator_1.IsNotEmpty)(),
-    __metadata("design:type", Number)
-], ComerConvEventDto.prototype, "announcementEventId", void 0);
-__decorate([
-    (0, class_validator_1.IsString)(),
-    (0, class_validator_1.MaxLength)(200),
-    (0, class_validator_1.IsOptional)(),
-    __metadata("design:type", String)
-], ComerConvEventDto.prototype, "description", void 0);
-__decorate([
-    (0, class_validator_1.IsDateString)(),
-    (0, class_validator_1.IsOptional)(),
-    __metadata("design:type", String)
-], ComerConvEventDto.prototype, "dates", void 0);
-__decorate([
-    (0, class_validator_1.IsString)(),
-    (0, class_validator_1.MaxLength)(70),
-    (0, class_validator_1.IsOptional)(),
-    __metadata("design:type", String)
-], ComerConvEventDto.prototype, "schedule", void 0);
-__decorate([
-    (0, class_validator_1.IsString)(),
-    (0, class_validator_1.MaxLength)(200),
-    (0, class_validator_1.IsOptional)(),
-    __metadata("design:type", String)
-], ComerConvEventDto.prototype, "place", void 0);
-__decorate([
-    (0, class_validator_1.IsString)(),
-    (0, class_validator_1.MaxLength)(500),
-    (0, class_validator_1.IsOptional)(),
-    __metadata("design:type", String)
-], ComerConvEventDto.prototype, "restriction", void 0);
 exports.ComerConvEventDto = ComerConvEventDto;
 //# sourceMappingURL=comer-agreement-events.dto.js.map
